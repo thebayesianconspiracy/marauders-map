c07d1cc2ea05638c12f73025ad5c8d559f7c3344
Soumyadeep Mukherjee <soumyadeep.mukherjee@appdynamics.com>
2018-01-21 01:32:00 +0530

    Change callMethod

--- src/Utils.js
+++ src/Utils.js
@@ -7,6 +7,13 @@ async function start() {
   const admin = rest.createUser('fernandes', 'lololotrol');
   const value = await co(admin);
   console.log('value is', value);
+  const set = 'set';
+  const args = {
+    storedData: '22'
+  };
+  console.log('Admin is', value);
+  const caller = await co(rest.callMethod(value,'SimpleStorage', set, args));
+  console.log('Caller is', caller);
 }
 
 export default {
