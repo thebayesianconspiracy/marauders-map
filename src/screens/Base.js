7dec97b4aaa726c221b508e2a4506b325748ffec
Sreejith Pp <mailz4sreejith@gmail.com>
2018-01-21 13:44:19 +0530

    styles change

--- src/screens/Base.js
+++ src/screens/Base.js
@@ -5,6 +5,29 @@ import _ from 'lodash';
 
 import { getLots } from '../actions/lots';
 
+const userinfo = {
+  flex: 1,
+  display: 'inline-block'
+};
+
+const lotDataStyle = {
+  flex: 1,
+  display: 'inline-block'
+};
+
+const lotRoot = {
+  'flex-direction': 'row',
+  flex: 1,
+  'align-items': 'center',
+  'justify-content': 'center',
+  display: 'inline-block',
+}
+
+const rootStyle = {
+  'flex-direction': 'column',
+  flex: 1
+};
+
 class Base extends React.Component {
 
   constructor(props) {
@@ -23,14 +46,20 @@ class Base extends React.Component {
     console.log('lots are', lots);
     const lotData = _.map(lots, lot => (
       <div>
-      Lot location: {lot.location}, date: {_.toString(new Date(lot.created))}
+        Lot location: {lot.location}, date: {_.toString(new Date(lot.created))}
       </div>
     ));
     return (
-      <div>
-        Welcome back, {username}
-        {lotData}
+      <div style={rootStyle}>
         <Link to="/addlots">Add lots</Link>
+        <div style={lotRoot}>
+          <div style={lotDataStyle}>
+            {lotData}
+          </div>
+          <div style={userinfo}>
+            Welcome back, {username}
+          </div>
+        </div>
       </div>
     );
   }
