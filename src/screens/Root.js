376ee2f643737c5fcb0c8626258e6524f0456e34
Sreejith Pp <mailz4sreejith@gmail.com>
2018-01-21 12:01:40 +0530

    Reset all files

--- src/screens/Root.js
+++ src/screens/Root.js
@@ -1,30 +1,47 @@
 import React, { Component } from 'react';
+import { Router, Route, Link } from 'react-router';
 import { connect } from 'react-redux';
 import _ from 'lodash';
-import store from '../store';
 
 import history from '../history';
 import Signup from './Signup';
 import Base from './Base';
 import LotAdd from './LotAdd';
 
-class Router extends Component {
+function requireAuth(username, nextState, replace) {
+  if (_.isEmpty(username)) {
+    replace({
+      pathname: '/login'
+    });
+  }
+}
+
+
+function requireNoAuth(username, nextState, replace) {
+  if (!_.isEmpty(username)) {
+    replace({
+      pathname: '/'
+    });
+  }
+}
+
+
+class Root extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
   
   render() {
-    const username = this.props.login.username;
+    const username = this.props.login.get('username');
 
-    if (_.isEmpty(username)) {
-      return (<Signup />);
-    }
-
-    const path = window.location.pathname;
-    
-    if (path.match(/addlots/i)) {
-      return(
-        <LotAdd />
-      )
-    }
-    return (<Base />)
+    return (
+      <Router history={history}>
+        <Route path="/login" component={Signup} onEnter={requireNoAuth.bind(this, username)} /> 
+        <Route path="/" component={Base} onEnter={requireAuth.bind(this, username)} />
+        <Route path="/addlots" component={LotAdd} onEnter={requireAuth.bind(this, username)} />
+      </Router>
+    );
   }
 }
 
@@ -34,4 +51,4 @@ const mapStateToProps = (state, ownProps) => {
   }
 }
 
-export default connect(mapStateToProps)(Router);
+export default connect(mapStateToProps)(Root);
